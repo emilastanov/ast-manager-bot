6fe54339ef1d6c03ae846414950b745e4620573a
emilastanov <emila1998@me.com>
2025-06-02 17:01:02 +0300

    fix

--- models/User.py
+++ models/User.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, BigInteger
+from datetime import datetime
+
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
 from enum import Enum as EnumBase
 
@@ -6,27 +9,27 @@ from .base import Base, CRUDMixin
 
 
 class UserType(EnumBase):
-    private = "private"
-    group = "group"
     supergroup = "supergroup"
     channel = "channel"
+    private = "private"
+    group = "group"
 
 
 class User(Base, CRUDMixin):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    chat_id = Column(BigInteger, unique=True, nullable=False)
-    chat_type = Column(Enum(UserType, name="user_type_enum"), nullable=False)
-    language_code = Column(String(10), nullable=True)
-    group_title = Column(String, nullable=True)
-    first_name = Column(String, nullable=True)
-    last_name = Column(String, nullable=True)
-    username = Column(String, nullable=True)
+    chat_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    chat_type: Mapped[UserType] = mapped_column(Enum(UserType, name="user_type_enum"), nullable=False)
+    language_code: Mapped[str] = mapped_column(nullable=True)
+    group_title: Mapped[str] = mapped_column(nullable=True)
+    first_name: Mapped[str] = mapped_column(nullable=True)
+    last_name: Mapped[str] = mapped_column(nullable=True)
+    username: Mapped[str] = mapped_column(nullable=True)
 
-    is_active = Column(Boolean, server_default='1', nullable=False)
-    is_admin = Column(Boolean, server_default='0', nullable=False)
+    is_active: Mapped[bool] = mapped_column(server_default="1", nullable=False)
+    is_admin: Mapped[bool] = mapped_column(server_default="0", nullable=False)
 
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
